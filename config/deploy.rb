c773ebbae560d5b7c1ff7e8943b6d723ba01f326
Awaru Ryudo <ryudoawaru@gmail.com>
2014-04-22 19:20:26 +0800

    加入touch restart.txt

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
     end
